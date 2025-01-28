494af6018184a115165d173e2774d1af8d101e40
bazil <zamanbazil123@gmail.com>
2025-01-28 17:09:20 +0530

    reverseString

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+//                                        LEETCODE QUESTION
+
+//58. Length of Last Word
+
 var lengthOfLastWord = function(s) {
     const word= s.split(' ').pop().length;
     return word;
 };
 console.log(lengthOfLastWord("Hello world"));
+
+
+
+//344. Reverse String
+
+//Input: s = ["h","e","l","l","o"]
+//tput: ["o","l","l","e","h"]
+
+var reverseString = function(s) {
+    return s.reverse();
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
