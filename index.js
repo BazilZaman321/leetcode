041c5b956fc359d6736e381c2a36247675ba9c85
bazil <zamanbazil123@gmail.com>
2025-03-07 14:37:23 +0530

    update

--- index.js
+++ index.js
@@ -55,3 +55,24 @@ var reverseWords = function(s) {
 
 console.log(mySqrt(4)); 
 console.log(mySqrt(8));
+
+
+// 412 FizzBuzz
+
+var fizzBuzz = function (n) {
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 == 0) {
+            result.push("Fizz");
+        } else if (i % 5 == 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+};
+
+console.log(fizzBuzz(3));
