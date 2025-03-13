020376f8ff81cfb45721539fb4986c9290dc7915
bazil <zamanbazil123@gmail.com>
2025-03-13 17:06:08 +0530

    update

---
+++ 1342.js
@@ -0,0 +1,16 @@
+//1342. Number of Steps to Reduce a Number to Zero
+
+
+var numberOfSteps = function(num) {
+    let steps = 0;
+    while (num > 0) {
+        num = num % 2 === 0 ? num / 2 : num - 1; 
+        steps++;
+    }
+    return steps;
+};
+ 
+
+console.log(numberOfSteps(14)); 
+console.log(numberOfSteps(8));  
+console.log(numberOfSteps(123)); 
