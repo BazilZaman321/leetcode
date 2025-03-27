7c49e346a2a09451ad655d8463c36ab0ceaa76d5
bazil <zamanbazil123@gmail.com>
2025-03-27 16:28:29 +0530

    update

--- 1929.js
+++ 1929.js
@@ -1,2 +1,10 @@
 //   1929. Concatenation of Array
 
+var getConcatenation = function(nums) {
+    return nums.concat(nums);
+};
+
+
+console.log(getConcatenation([1,2,1])); 
+console.log(getConcatenation([1,3,2,1]));
+console.log(getConcatenation([]));
