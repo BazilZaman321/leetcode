789d3b574b51f47ffe92dad7232ec4f49a557791
bazil <zamanbazil123@gmail.com>
2025-03-28 10:39:10 +0530

    update

--- 2469.js
+++ 2469.js
@@ -1,1 +1,10 @@
 //    2469. Convert the Temperature
+
+var convertTemperature = function(celsius) {
+    return [celsius + 273.15, celsius * 1.80 + 32.00]; 
+};
+
+
+console.log(convertTemperature(36.50));
+console.log(convertTemperature(122.11)); 
+console.log(convertTemperature(0)); 
