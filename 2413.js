f7b49e282e7f7747f30ab9ce287af13395c6fa0c
bazil <zamanbazil123@gmail.com>
2025-04-24 10:57:22 +0530

    update

--- 2413.js
+++ 2413.js
@@ -1,2 +1,11 @@
 //       2413. Smallest Even Multiple
 
+var smallestEvenMultiple = function(n) {
+    return n % 2 === 0 ? n : n * 2;
+};
+
+
+console.log(smallestEvenMultiple(5)); 
+console.log(smallestEvenMultiple(6)); 
+console.log(smallestEvenMultiple(7)); 
+console.log(smallestEvenMultiple(10));
---
+++ 509.js
@@ -0,0 +1,20 @@
+//  509. Fibonacci Number
+
+
+var fib = function (n) {
+    if (n === 0) return 0;
+    if (n == 1) return 1;
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+};
+
+
+console.log(fib(4));
